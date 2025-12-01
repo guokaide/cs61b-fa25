aab6f7e53b238da83edd00a367dbec637482997f
kaide <1378560653@qq.com>
2025-12-01 18:45:05 +0800

    finish hw05 Stack without SLList

--- hw05/src/Stack.java
+++ hw05/src/Stack.java
@@ -1,29 +1,50 @@
 public class Stack {
 
-    private final SLList list;
+    private static class IntNode {
+        // 在 Java 中，外部类与其嵌套类互相可以访问对方的私有成员；编译器会生成必要的合成访问器。
+        private int item;
+        private IntNode next;
+
+        public IntNode(int i, IntNode n) {
+            this.item = i;
+            this.next = n;
+        }
+    }
+
+    private final IntNode sentinel;
+    private int size;
+    private int sum;
 
     public Stack() {
-        this.list = new SLList();
+        this.sentinel = new IntNode(0, null);
+        this.size = 0;
+        this.sum = 0;
     }
 
     /** Puts x on top of the stack. */
     public void push(int x) {
-        list.addFirst(x);
+        this.sentinel.next = new IntNode(x, this.sentinel.next);
+        this.size++;
+        this.sum += x;
     }
 
     /** Removes and returns the top item on the stack. */
     public int pop() {
-        return list.removeFirst();
+        IntNode top = this.sentinel.next;
+        this.sentinel.next = top.next;
+        this.size--;
+        this.sum -= top.item;
+        return top.item;
     }
 
     /** Returns the number of items on the stack. */
     public int size() {
-        return list.size();
+        return this.size;
     }
 
     /** Computes the sum of the numbers on the stack. */
     public int sum() {
-        return list.sum();
+        return this.sum;
     }
 
 }
